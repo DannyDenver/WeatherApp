2e5fc11383e96f3072211f4aff75fb8065ea356d
DannyDenver <djt09d@fulbrightmail.org>
2017-09-16 17:56:05 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+﻿"use strict"
+var key = "39556bab6d5e992ec4015b2b038c80c4"; 
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather(){
+  var searchCity = city.value;
+  loadingText.style.display = "block";
+  
+  setTimeout(function(){
+      if (searchCity.trim().length == 0 ){ 
+    alert("please enter a city");
+  } 
+      var http = new XMLHttpRequest();
+      var method= 'GET'; 
+      var url = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+      var requestURL= url + searchCity.trim() + "&units=metric&appid=" + key;
+      
+      http.open(method, requestURL);
+      http.onreadystatechange = function(){
+        if(http.readyState == XMLHttpRequest.DONE && http.status === 200){
+          var data = JSON.parse(http.responseText);
+          var weatherData = new Weather(searchCity, data.weather[0].description.toUpperCase());
+          weatherData.temperature = data.main.temp;
+          console.log(weatherData);
+          
+          updateWeather(weatherData);
+          
+          
+        }else if(http.readyState === XMLHttpRequest.DONE){
+          alert("Something went wrong.")
+        }
+  }
+  http.send();
+  loadingText.style.display = "none";
+    
+  }, 2000);
+}
+  
+function updateWeather(weatherData){
+  weatherCity.textContent = weatherData.cityName;
+  weatherDescription.textContent= weatherData.description;
+  weatherTemperature.textContent = weatherData.temperature
+
+  weatherBox.style.display = "block";
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ init.js
@@ -0,0 +1,14 @@
+﻿"use strict"
+
+var searchButton = document.querySelector('button');
+// document.querySelector('button')
+
+var city = document.querySelector('#city');
+
+var loadingText= document.querySelector("#load");
+var weatherBox = document.querySelector("#weather");
+
+
+var weatherCity = weatherBox.firstElementChild;
+var weatherTemperature = document.querySelector("#weatherTemperature")
+var weatherDescription = weatherBox.lastElementChild;
